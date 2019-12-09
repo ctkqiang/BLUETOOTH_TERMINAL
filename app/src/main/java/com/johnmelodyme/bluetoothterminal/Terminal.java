e105fbde94c0b93f1f4c7c68d1c2dff34473f6ce
John Melody Melissa <Johnmelodyme@yandex.com>
2019-12-09 10:45:42 +0800

    Updated Bluetooth Adapter;

--- app/src/main/java/com/johnmelodyme/bluetoothterminal/Terminal.java
+++ app/src/main/java/com/johnmelodyme/bluetoothterminal/Terminal.java
@@ -3,6 +3,8 @@ package com.johnmelodyme.bluetoothterminal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,6 +19,7 @@ public class Terminal extends AppCompatActivity {
     // DECLARATION :
     int REQUEST_CODE;
     BluetoothAdapter BA;
+    BluetoothManager BM;
     {
         REQUEST_CODE = 1;
     }
@@ -34,6 +37,18 @@ public class Terminal extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init_DECLARATION();
         CHECK_BLUETOOTH();
+        ON_BLUETOOTH();
+    }
+
+    private void ON_BLUETOOTH() {
+        if (!(BA.isEnabled())){
+            BM.getAdapter()
+                    .enable();
+        }
+        else {
+            String message = "BLUETOOTH ERROR";
+            System.out.println(message);
+        }
     }
 
     private void CHECK_BLUETOOTH() {
